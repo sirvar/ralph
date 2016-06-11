4003f27731ed3550f106b887872b2690deee8f98
Rikin Katyal <katyalrikin@gmail.com>
2016-06-10 20:53:18 -0400

    added options

--- lib/main.py
+++ lib/main.py
@@ -15,6 +15,8 @@ username = creds[4]
 
 api = tweepy.API(auth)
 
+options = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes, definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Yes', 'Signs point to yes', 'Reply hazy try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again', "Don't count on it", 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful']
+
 class mStreamListener(tweepy.StreamListener):
 	def on_status(self, status):
 		if not self.tweetByMe(status.user.id):
